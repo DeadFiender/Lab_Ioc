93e2a60cd90d6dab55e25447aa21ae83140f70dc
DeadFiender <artem.bibekin@gmail.com>
2025-06-05 18:24:27 +0700

    add comments

--- IDataReader.cpp
+++ IDataReader.cpp
@@ -7,10 +7,10 @@ const QVector<QString> IDataReader::FORMAT_DATE = {
     "yyyy-MM-dd HH:mm", "yyyy-MM-dd"
 };
 // Статический вектор строк, хранящий возможные форматы дат.
-// Используется для парсинга дат в методе `parseDate`.
+// Используется для парсинга дат
 
 QDateTime IDataReader::parseDate(const QString& raw) const {
-    for (const auto& format : FORMAT_DATE) {
+    for (const auto& format : FORMAT_DATE) {//проходим по всем форматам дат из вектора
         QDateTime dt = QDateTime::fromString(raw, format);
         if (dt.isValid()) return dt;
         // Попытка преобразовать строку в объект `QDateTime` с использованием каждого формата.
@@ -18,11 +18,11 @@ QDateTime IDataReader::parseDate(const QString& raw) const {
     }
 
     // Альтернатива: "date minutes"
-    const auto parts = raw.split(' ');
-    if (parts.size() == 2) {
-        QDate d = QDate::fromString(parts[0], "dd.MM.yyyy");
-        bool ok = false;
-        int mins = parts[1].toInt(&ok);
+    const auto parts = raw.split(' ');//Строка raw разбивается на части по пробелу
+    if (parts.size() == 2) {//Проверяется, что строка содержит ровно две части
+        QDate d = QDate::fromString(parts[0], "dd.MM.yyyy");//Первая часть строки преобразуется в дату
+        bool ok = false;//проверка успешности преобразования
+        int mins = parts[1].toInt(&ok);//минуты в целое число
         if (d.isValid() && ok)
             return d.startOfDay().addSecs(mins * 60);
         // Если строка имеет формат "дата минуты", преобразуем её в `QDateTime`.
